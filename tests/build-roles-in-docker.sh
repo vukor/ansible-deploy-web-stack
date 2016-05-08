e1e9d67d0941709c5b28e3b8fffb9ebda0985f37
Anton Bugreev <anton@bugreev.ru>
2016-05-08 23:00:51 +0600

    Added tests for CentOS 6/7

--- tests/build-roles-in-docker.sh
+++ tests/build-roles-in-docker.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-## Install ansible in offical docker image centos:centos6
-rpm -Uvh https://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-yum install ansible1.9 -y
+case "$1" in
+  'centos6' ) version="6" ;;
+  'centos7' ) version="7" ;;
+  *	    ) echo "Incorrect arg, use 'centos6' or 'centos7'." && exit 1;;
+esac
 
+## Install ansible in offical docker image centos 6/7
+if [ ${version} == "6" ]; then
+  rpm -Uvh https://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+else
+  rpm -Uvh https://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm state=present
+fi
+
+## Install ansible
+yum install ansible1.9 -y
+  
 ## Build on localhost
-echo 'localhost ansible_connection=local prj_host=centos6.local' >> inventory/hosts
+echo "localhost ansible_connection=local prj_host=centos${version}.local" >> inventory/hosts
 
 ## Disable iptables (fail build in docker)
 sed -i '/iptables/d' playbook/setup.yml
@@ -14,7 +26,7 @@ sed -i '/iptables/d' playbook/setup.yml
 ansible-playbook --timeout=60 playbook/setup.yml -t setup
 
 ## Test project
-curl --silent http://centos6.local/ |grep 'Web application centos6.local is up.' || exit 1
+curl --silent http://${1}.local/ |grep "Web application ${1}.local is up." || exit 1
 
 exit 0
 
